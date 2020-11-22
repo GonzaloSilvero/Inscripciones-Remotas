8fc99c9bd7e8d03347939e387f5c2a510febb8be
Nicolas Giglio <nicosgiglio@gmail.com>
2020-11-22 18:46:24 -0300

    Update tutor.php

--- tutor.php
+++ tutor.php
@@ -6,33 +6,34 @@
 </head>
 <body>		
 
-	<form class="form-signin">
+	<form class="form-signin" method="POST">
             <center>
             <h1 class="h1 mb-3 font-weight-normal">Registro</h1>
             <label for="inputNombre" class="sr-only">Nombre</label>
-            <input type="text" id="inputNombre" class="form-control" placeholder="Nombre" required autofocus>
-            <label for="inputApellido" class="sr-only">Apellido</label>
-			<input type="text" id="inputApellido" class="form-control" placeholder="Apellido" required>
+			<input type="text" name="nombre_T" id="inputNombre" class="form-control" placeholder="Nombre" required autofocus>
+			
+            <label for="inputApellido"  class="sr-only">Apellido</label>
+			<input type="text" name="apellido_T" id="inputApellido" class="form-control" placeholder="Apellido" required>
+
 			<label for="inputTel" class="sr-only">Telefono</label>
-            <input type="text" id="inputTel" class="form-control" placeholder="Telefono" required autofocus>
+			<input type="text" name="telefono_T" id="inputTel" class="form-control" placeholder="Telefono" required autofocus>
+			
             <label for="inputEmail" class="sr-only">Email</label>
-			<input type="text" id="inputEmail" class="form-control" placeholder="Email" required>
+			<input type="email" name="email_T" id="inputEmail" class="form-control" placeholder="Email" required>
+
 			<label for="inputUsuario" class="sr-only">Usuario</label>
-            <input type="text" id="inputUsuario" class="form-control" placeholder="Dni Tutor" required autofocus>
+			<input type="text" name="usuario_T" id="inputUsuario" class="form-control" placeholder="Dni Tutor" required autofocus>
+			
             <label for="inputContraseña" class="sr-only">Contraseña</label>
-            <input type="text" id="inputContraseña" class="form-control" placeholder="Dni Alumno" required>
+			<input type="text" name="contrasena_T" id="inputContraseña" class="form-control" placeholder="Dni Alumno" required>
+			
             <button class="btn btn-lg btn-secondary btn-block" type="submit">Ingresar</button>
             </center>
         </form>
 
 	<?php
-
-	$conexion =mysqli_connect("localhost", "root", "","sorteo");
-
-
-	//require_once 'conexiones.php';
 		if (isset($_POST['ida'])) {
-
+			$conexion = mysqli_connect("localhost", "root", "","sorteo");
 			//Recoger los valores del formulario de registro
 			$nombre_T= $_POST['nombre_T'];
 			$apellido_T= $_POST['apellido_T'];
