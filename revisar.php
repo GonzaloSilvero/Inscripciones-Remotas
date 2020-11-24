df63698ced3da87460fce71918c7f99879fe491d
Nicolas Giglio <nicosgiglio@gmail.com>
2020-11-24 20:14:18 -0300

    vista de datos del alumno en proceso

--- revisar.php
+++ revisar.php
@@ -6,46 +6,51 @@
 </head>
 <body>		
 
-	<form class="form-signin" action="tutor.php" method="POST">
+<?php
+	session_start();
+	if (!isset($_SESSION["usuario"])) {
+        header("location:index.php");
+    }
+    else if (!isset($_SESSION['id_alumno'])) {
+        header("location:hub.php");
+    }
+
+    $conexion =mysqli_connect("localhost", "root", "","sorteo");
+
+    $referenciaAlumno = $_SESSION['id_alumno'];
+
+	$resultado_alumno= mysqli_query($conexion, "SELECT * FROM `chicos_i` WHERE `ID` = '$referenciaAlumno' ");
+    $datosAlumno= mysqli_fetch_assoc($resultado_alumno);
+
+    $datosAlumno['Nombre'];
+    $datosAlumno['Apellido'];
+    $datosAlumno['Edad'];
+    $datosAlumno['dni'];
+    $datosAlumno['Domicilio'];
+    $datosAlumno['Escuela_A'];
+	?>
+
+
+	<div class="form-signin">
             <center>
-            <h1 class="h1 mb-3 font-weight-normal">Registro</h1>
-			<input type="text" id="inputNombre" class="form-control" name="nombre_T" placeholder="Nombre" required autofocus disabled >
+            <h1 class="h1 mb-3 font-weight-normal">Datos del Alumno</h1>
+			<input type="text" id="inputNombre" class="form-control" placeholder="Nombre: " value="<?php echo "Nombre: ".$datosAlumno['Nombre']; ?>" disabled >
 			
-			<input type="text" id="inputApellido" class="form-control" name="apellido_T" placeholder="Apellido" required disabled>
+			<input type="text" id="inputApellido" class="form-control" placeholder="Apellido" value="<?php echo "Apellido: ".$datosAlumno['Apellido']; ?>" disabled>
 
-			<input type="tel" id="inputTel" class="form-control" name="telefono_T" maxlength="10" placeholder="Telefono" required autofocus disabled>
+			<input type="tel" id="inputTel" class="form-control" maxlength="10" placeholder="Telefono" value="<?php echo "Edad: ".$datosAlumno['Edad']; ?>" disabled>
 			
-			<input type="email" id="inputEmail" class="form-control" name="email_T" placeholder="Email" required disabled>
+			<input type="email" id="inputEmail" class="form-control" placeholder="Email" value="<?php echo "DNI: ".$datosAlumno['dni']; ?>" disabled>
 
-			<input type="text" id="inputUsuario" class="form-control" name="usuario_T" minlength="8" maxlength="8" placeholder="Dni Tutor" required autofocus disabled>
+			<input type="text" id="inputUsuario" class="form-control" minlength="8" maxlength="8" placeholder="Dni Tutor" value="<?php echo "Domicilio: ".$datosAlumno['Domicilio']; ?>" disabled>
 			
-			<input type="text" id="inputContraseña" class="form-control" name="contrasena_T" minlength="8" maxlength="8" placeholder="Dni Alumno" required disabled>
+			<input type="text" id="inputContraseña" class="form-control" minlength="8" maxlength="8" placeholder="Dni Alumno" value="<?php echo "Escuela_A: ".$datosAlumno['Escuela_A']; ?>" disabled>
 			
-            <button class="btn btn-lg btn-secondary btn-block" name="ida" type="submit">Volver</button>
+			<a href="hub.php"> <button class="btn btn-lg btn-secondary btn-block" >Volver</button></a>
             </center>
-        </form>
+        </div>
 	<?php
-
-	$conexion =mysqli_connect("localhost", "root", "","sorteo");
-
-
-	//require_once 'conexiones.php';
-		if (isset($_POST['ida'])) {
-			
-			//Recoger los valores del formulario de registro
-			$nombre_T= $_POST['nombre_T'];
-			$apellido_T= $_POST['apellido_T'];
-			$telefono_T= $_POST['telefono_T'];
-			$Email_T= $_POST['email_T'];
-			$Usuario_T= $_POST['usuario_T'];
-			$contrasena_T= $_POST['contrasena_T'];
-			//$contrasena_T_codificada=password_hash($contrasena_T, PASSWORD_DEFAULT);
-			$consulta="INSERT INTO `tutor` (`Nombre`, `Apellido`, `Telefono`, `Usuario`, `Gmail`, `Contraseña`) VALUES ('$nombre_T', '$apellido_T', '$telefono_T', '$Usuario_T', '$Email_T', '$contrasena_T');";
-			$resultado= mysqli_query($conexion, $consulta);
-
-			header("Location: index.php");
-			die();
-		}
+	 mysqli_close($conexion);
 		include 'footer.html';
 	?>
 
