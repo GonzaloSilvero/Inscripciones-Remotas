8d007574e464eb1beb5cec2b8da36e864e18858d
GonzaloSilvero <gonzalosilvero02@gmail.com>
2020-11-25 04:32:07 -0300

    en modiicar tambien

--- panelAdmin/modificar.php
+++ panelAdmin/modificar.php
@@ -9,6 +9,26 @@
 </head>
 <body>		
 
+<header>
+<nav class="navbar navbar-expand navbar-dark bg-dark" >
+    <a class="navbar-brand" href="#"><i class="fas fa-door-open"></i></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample02">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+        </li>
+        <li class="nav-item">
+        </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+            <input class="form-control" type="text" placeholder="Search">
+        </form>
+    </div>
+</nav>
+
 <?php
 	session_start();
     if (!isset($_SESSION["admin"]))
